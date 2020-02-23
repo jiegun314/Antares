30b284eb43d530b5f77fbcf36a4952e7ff4ccb39
Jeffrey-JNJ <jeffrey314@163.com>
2020-02-23 15:33:30 +0800

    update master data import module to import SAP Price and Phoenix Tracking Table

--- antares/data_import.py
+++ antares/data_import.py
@@ -113,25 +113,31 @@ class DataInput:
             code_list.append(str(item[0]))
         # 测试阶段返回N个数字
         return code_list
-    
-    def import_master_data(self):
-        file_name = self.__class__.bu_name + "_Master_Data"
+
+    # import BU base master data
+    def import_master_data(self, data_type):
+        # for TU. data_type = {"Master_Data", "SAP_Price", "Phoenix_List"}
+        file_name = self.__class__.bu_name + "_" + data_type
         file_fullname = self.__class__.file_path + file_name + ".xlsx"
-        db_fullname = self.__class__.db_path + file_name + ".db"
-        print("开始读取文件")
+        db_fullname = self.__class__.db_path + self.__class__.bu_name + "_Master_Data.db"
+        print("~ Start to read the data file %s" %file_name)
         start_time = datetime.now()
         df = pd.read_excel(file_fullname, na_values="0")
-        data = df.values
+        # data = df.values
         stop_time = datetime.now()
-        print("文件读取完成，耗时%s秒" % (stop_time-start_time).seconds)
+        print("~ File reading complete，with time of %s seconds" % (stop_time-start_time).seconds)
         # 写入数据库
+        # conn = sqlite3.connect(db_fullname)
+        # conn.execute("DROP TABLE IF EXISTS " + file_name)
+        # new_tbl = db.DatabaseSetup(self.__class__.bu_name)
+        # new_tbl.create_master_data()
+        # conn.executemany("INSERT INTO " + file_name + " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", data)
+        # conn.commit()
+        # conn.close()
         conn = sqlite3.connect(db_fullname)
-        conn.execute("DROP TABLE IF EXISTS " + file_name)
-        new_tbl = db.DatabaseSetup(self.__class__.bu_name)
-        new_tbl.create_master_data()
-        conn.executemany("INSERT INTO " + file_name + " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", data)
-        conn.commit()
-        conn.close()
+        df.to_sql(name=file_name, con=conn, if_exists='replace', index=False)
+        print("%s is imported" % data_type)
+        pass
 
     def import_public_master_data(self):
         # print title
@@ -177,7 +183,7 @@ class DataInput:
 
 if __name__ == "__main__":
     data_input = DataInput("TU")
-    data_input.import_public_master_data()
+    data_input.import_master_data("Master_Data")
     # cmd = int(input("选择需要导入的数据，1 - GTS，2 - LP Sales， 3 - IMS, 4 - LP_INV: "))
     # if cmd == 1:
     #     data_input.sales_input ("GTS")
