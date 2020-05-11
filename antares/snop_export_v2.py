67eae78431890d747d824d244008f623b76043a7
周辉 <jeffrey314@163.com>
2020-05-11 23:20:10 +0800

    update export module

--- antares/snop_export_v2.py
+++ antares/snop_export_v2.py
@@ -31,32 +31,47 @@ class SNOPExportV2:
         # 测试阶段返回N个数字
         return code_list
 
+    def get_all_code_list(self):
+        database_fullname = self.__class__.db_path + self.__class__.bu_name + '_Master_Data.db'
+        datasheet_name = self.__class__.bu_name + '_Master_Data_Demo'
+        conn = sqlite3.connect(database_fullname)
+        sql_cmd = "SELECT Material FROM " + datasheet_name
+        df = pd.read_sql(sql=sql_cmd, con=conn)
+        code_list = df['Material'].values.tolist()
+        return code_list
+
     # Return all the sales data
-    def get_code_sales_list(self, sales_type):
+    def get_code_sales_list(self, sales_type, month_list):
+        str_month_list = ''
+        for month_item in month_list:
+            str_month_list += '\"' + month_item + '\",'
+        str_month_list = str_month_list.rstrip(',')
         file_name = self.__class__.bu_name + "_" + sales_type
         db_fullname = self.__class__.db_path + file_name + ".db"
         conn = sqlite3.connect(db_fullname)
-        c = conn.cursor()
         sql_cmd = "SELECT Material, Month, Quantity, Value_Standard_Cost, Value_SAP_Price from " + file_name + \
-                  " GROUP by Material, Month Order by Month"
-        c.execute(sql_cmd)
-        result = c.fetchall()
+                  " WHERE Month IN (" + str_month_list + ") GROUP by Material, Month Order by Month"
+        df = pd.read_sql(sql=sql_cmd, con=conn)
         conn.commit()
         conn.close()
-        return result
+        return df
 
     # Return all the Inventory Data
-    def get_code_inventory_list(self, inv_type):
+    def get_code_inventory_list(self, inv_type, month_list):
+        str_month_list = ''
+        for month_item in month_list:
+            str_month_list += '\"' + month_item + '\",'
+        str_month_list = str_month_list.rstrip(',')
         file_name = self.__class__.bu_name + "_" + inv_type
         db_fullname = self.__class__.db_path + file_name + ".db"
         conn = sqlite3.connect(db_fullname)
         c = conn.cursor()
         if inv_type == "JNJ_INV":
             sql_cmd = "SELECT Material, Month, sum(Available_Stock), sum(Value_Standard_Cost), sum(Value_SAP_Price)" \
-                      " from " + file_name + " GROUP BY Material, Month ORDER BY Month"
+                      " from " + file_name + " WHERE Month IN (" + str_month_list + ") GROUP BY Material, Month ORDER BY Month"
         else:
             sql_cmd = "SELECT Material, Month, sum(Quantity), sum(Value_Standard_Cost), sum(Value_SAP_Price) from " \
-                      + file_name + " GROUP BY Material, Month ORDER BY Month"
+                      + file_name + " WHERE Month IN (" + str_month_list + ") GROUP BY Material, Month ORDER BY Month"
         result = c.execute(sql_cmd).fetchall()
         conn.commit()
         conn.close()
@@ -70,34 +85,24 @@ class SNOPExportV2:
         month_list = info_check.get_time_list(current_month, -24)
         # print(month_list)
         # Get active code list
-        active_code_list = self.get_active_codes()
+        active_code_list = self.get_all_code_list()
         print("Data Length:", len(active_code_list))
         # Generate sales type list
         sales_type = ["GTS", "LPSales", "IMS"]
         lst_sales_df = []
         for sales_type_item in sales_type:
-            sales_list = self.get_code_sales_list(sales_type_item)
-            # Generate Dict
-            df = pd.DataFrame(sales_list)
-            df.rename(
-                columns={0: "Material", 1: "Month", 2: "Quantity", 3: "Value_Standard_Cost", 4: "Value_SAP_Price"},
-                inplace=True)
-            # Generate pivot_table
+            df = self.get_code_sales_list(sales_type_item, month_list)
             pivot_result = pd.pivot_table(df, index="Material",
                                           values=["Quantity", "Value_Standard_Cost", "Value_SAP_Price"],
                                           columns="Month", fill_value=0)
+            print(list(pivot_result))
             print(sales_type_item, " is ready!")
             lst_sales_df.append(pivot_result)
         # Generate inventory type list
         inventory_type = ["JNJ_INV", "LP_INV"]
         lst_inv_df = []
         for inv_type_item in inventory_type:
-            inventory_list = self.get_code_inventory_list(inv_type_item)
-            # Generate Dict
-            df = pd.DataFrame(inventory_list)
-            df.rename(
-                columns={0: "Material", 1: "Month", 2: "Quantity", 3: "Value_Standard_Cost", 4: "Value_SAP_Price"},
-                inplace=True)
+            df = self.get_code_inventory_list(inv_type_item, month_list)
             # Generate pivot_table
             pivot_result = pd.pivot_table(df, index="Material",
                                           values=["Quantity", "Value_Standard_Cost", "Value_SAP_Price"],
