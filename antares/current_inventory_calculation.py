e831cc62a3b11579e902035f27bc1e342fe9ecab
Jeffrey-JNJ <hzhou23@its.jnj.com>
2021-03-20 17:57:36 +0800

    modify: finalize oneclick sync with new database

--- antares/current_inventory_calculation.py
+++ antares/current_inventory_calculation.py
@@ -11,13 +11,13 @@ class CurrentInventoryCalculation:
     backorder_path = "../data/_Backorder/"
     inventory_path = "../data/_INV_Export/"
     update_file_path = "../data/_Update/"
-    currency_rate = 7.0842
+    oneclick_database = db_path + 'oneclick_dps_inventory.db'
+    currency_rate = 6.9233
     _sync_days = 0
     _exception_list = []
 
-    def __int__(self, bu_input):
-        self.__class__.bu_name = bu_input
-
+    def __int__(self):
+        pass
 
     @property
     def sync_days(self):
@@ -49,21 +49,23 @@ class CurrentInventoryCalculation:
         # select dps products
         df_dps = df.loc[(df['Business_Group'] == 'Orthopedics') & (df['Loc'] == "Total")]
         # map with JT local Hierarchy
-        conn = sqlite3.connect(current_app.config['DATABASE_JOINT_MASTER_DATA'])
-        local_hierarchy_datasheet = 'JT_Local_Hierarchy'
-        sql_cmd = 'SELECT Material, Local_Hierarchy FROM ' + local_hierarchy_datasheet
-        df_local_hierarchy = pd.read_sql(sql=sql_cmd, con=conn, index_col='Material')
+        sql_cmd = 'SELECT Material, Local_Hierarchy FROM JT_Local_Hierarchy'
+        _conn = sqlite3.connect(self.db_path + 'JT_Master_Data.db')
+        df_local_hierarchy = pd.read_sql(sql=sql_cmd, con=_conn, index_col='Material')
         df_dps = df_dps.join(df_local_hierarchy)
         df_dps.loc[df_dps['Local_Hierarchy'].notnull(), 'Hierarchy_5'] = df_dps['Local_Hierarchy']
         df_dps.drop(columns=['Local_Hierarchy'], inplace=True)
         df_dps.reset_index(inplace=True)
+        # add date to first column
+        df_dps.insert(0, 'Date', str_date)
         # export to database
-        conn = sqlite3.connect(current_app.config['DATABASE_CURRENT_INVENTORY_DPS'])
-        df_dps.to_sql("INV" + str_date, con=conn, if_exists="replace", index=False)
+        _conn = sqlite3.connect(self.db_path + 'oneclick_dps_inventory.db')
+        df_dps.to_sql('oneclick_inventory', con=_conn, if_exists="append", index=False)
+        print('%s was imported.' % str_date)
         return 1
 
     # synchronize inventory data with oneclick
-    def start_synchronize(self):
+    def start_synchronize(self) -> list:
         lst_folder_temp = []
         import_success_count, import_fail_count = 0, 0
         # read folder list in share folder
@@ -76,13 +78,12 @@ class CurrentInventoryCalculation:
         lst_folder_temp.sort()
         lst_folder_sharepoint = lst_folder_temp[0 - self._sync_days:]
         # get local data
-        lst_db_date = self.get_local_inventory_table_list()
-        lst_current_database = [item.lstrip("INV") for item in lst_db_date]
+        lst_current_database = self.get_local_inventory_date_list()
         # start to sync
         # delete outdated data
         for item in lst_current_database:
             if item not in lst_folder_sharepoint:
-                self.delete_local_inventory_table(item)
+                self.delete_local_inventory_item(item)
                 lst_current_database.remove(item)
         # import new data
         for item in lst_folder_sharepoint:
@@ -92,26 +93,45 @@ class CurrentInventoryCalculation:
                     import_success_count += 1
                 else:
                     import_fail_count += 1
-        most_updated_table = self.get_local_inventory_table_list()[-1]
+        most_updated_table = self.get_local_inventory_date_list()[-1][0]
         return [import_success_count, import_fail_count, most_updated_table]
 
-    @staticmethod
-    def get_local_inventory_table_list():
-        conn = sqlite3.connect(current_app.config['DATABASE_CURRENT_INVENTORY_DPS'])
-        c = conn.cursor()
-        c.execute("select name from sqlite_master where type='table' order by name")
-        table_list = [item[0] for item in c.fetchall()]
-        conn.commit()
-        conn.close()
-        return table_list
+    def get_local_inventory_date_list(self) -> list:
+        _conn = sqlite3.connect(self.db_path + 'oneclick_dps_inventory.db')
+        c = _conn.cursor()
+        c.execute('SELECT DISTINCT(Date) FROM oneclick_inventory ORDER BY Date')
+        result = c.fetchall()
+        date_list = [item[0] for item in result]
+        return date_list
 
     # inventory_data: YYYYMMDD
-    @staticmethod
-    def delete_local_inventory_table(inventory_date):
-        table_name = 'INV' + inventory_date
-        conn = sqlite3.connect(current_app.config['DATABASE_CURRENT_INVENTORY_DPS'])
-        c = conn.cursor()
-        sql_cmd = "DROP TABLE " + table_name
+    def delete_local_inventory_item(self, inventory_date):
+        _conn = sqlite3.connect(self.db_path + 'oneclick_dps_inventory.db')
+        c = _conn.cursor()
+        sql_cmd = 'DELETE FROM oneclick_inventory WHERE Date = \"%s\"' % inventory_date
         c.execute(sql_cmd)
-        conn.commit()
-        conn.close()
+        _conn.commit()
+        _conn.close()
+
+    # check if this date exist (date format YYYYMMDD)
+    def check_date_availability(self, str_date_input):
+        _conn = sqlite3.connect(self.oneclick_database)
+        c = _conn.cursor()
+        c.execute('SELECT count(Material) FROM oneclick_inventory WHERE Date = \"%s\"' % str_date_input)
+        result = c.fetchone()[0]
+        return True if result else False
+
+    def get_newest_date(self) -> str:
+        _conn = sqlite3.connect(self.oneclick_database)
+        c = _conn.cursor()
+        c.execute('SELECT DISTINCT(Date) FROM oneclick_inventory ORDER BY Date DESC LIMIT 1')
+        str_newest_date = c.fetchone()[0]
+        return str_newest_date
+
+
+if __name__ == "__main__":
+    sync_test = CurrentInventoryCalculation()
+    sync_test.sync_days = 90
+    sync_test.exception_list = []
+    sync_test.start_synchronize()
+    pass
